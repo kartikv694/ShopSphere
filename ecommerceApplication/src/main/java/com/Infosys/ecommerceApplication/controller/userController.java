e9297dde6905b0a34bf7ed1430d80bd25d5097ee
Kartik Verma <kartikverma694@gmail.com>
2026-04-20 01:33:09 +0530

    Backend: add Encryption

--- ecommerceApplication/src/main/java/com/Infosys/ecommerceApplication/controller/userController.java
+++ ecommerceApplication/src/main/java/com/Infosys/ecommerceApplication/controller/userController.java
@@ -15,6 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.Infosys.ecommerceApplication.model.User;
 import com.Infosys.ecommerceApplication.service.userService;
 
+// Frontend Connect
 @CrossOrigin(origins = "http://localhost:5173")
 @RestController
 @RequestMapping("api/auth")
