dfacd0d7fcb7175aa242602b199b0541fc0c4980
Kartik Verma <kartikverma694@gmail.com>
2026-04-20 01:24:24 +0530

    Backend: add Encryption

--- ecommerceApplication/src/main/java/com/Infosys/ecommerceApplication/config/SecurityConfig.java
+++ ecommerceApplication/src/main/java/com/Infosys/ecommerceApplication/config/SecurityConfig.java
@@ -5,6 +5,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 	
+// Security Checks
+
 @Configuration
 public class SecurityConfig {
 
